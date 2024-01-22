f5d2e90e53a2afafff08030524aa0acde47fa157
Vaibhav Balasaheb Deshmukh <156877803+deshmukhvaibhav2001@users.noreply.github.com>
2024-01-22 09:22:36 +0000

    container config done

---
+++ basics/02_datatype.js
@@ -0,0 +1,19 @@
+let num1=18;
+let str ="hello world"
+let isOld=false;
+let age=null;
+let bigBin = BigInt("0b1010101001010101001111111111111111");
+
+console.log(typeof  bigBin)
+console.log(typeof num1)
+console.log(typeof str)
+console.log(typeof isOld)
+console.log(typeof age)
+
+
+// number => 2 to power 53
+// String=>""
+// bnoolean=>true/false
+// null=>standalone value
+// undefined=>
+// Symbol =>unique
---
+++ basics/03_conversionoperation.js
@@ -0,0 +1,42 @@
+let score = "33"
+
+console.log( typeof score)
+
+let valueInNumber=Number(score)
+console.log( typeof valueInNumber)
+console.log(valueInNumber)
+
+//"33"=> 33
+//"33abc"=> NaN
+// true =>1; false=>0
+
+let isLoggedIn=1
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1=true; 0=false
+// ""= false
+// "vaibhav" =true
+
+let num=45
+
+let stringNumber=String(num)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+//*********************Operation **************/
+
+let str1="hello"
+let str2=" vaibhav"
+
+let str3 =str1 +str2
+console.log(str3)
+
+console.log("1" + 2)
+console.log( 1+ "2")
+console.log("1" +2+ 2)
+console.log(1 + 2 + "2")
+
+console.log(+true)
+console.log(+"")
