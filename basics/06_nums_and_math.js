07ffc8b875d63df0a5ece3359e0e0ed3b70b29e9
Vaibhav Balasaheb Deshmukh <156877803+deshmukhvaibhav2001@users.noreply.github.com>
2024-01-23 10:54:32 +0000

    container config done

---
+++ basics/06_nums_and_math.js
@@ -0,0 +1,34 @@
+// // Numbers 
+
+// const score =400
+// console.log(score)
+
+// const balance=new Number(2345566866)
+// console.log(balance)
+
+// // methods in number 
+// //1] tostring()
+
+// const money=345678
+// console.log(money.toString());
+
+// //  2] toExponential()
+
+// const a=9.3456
+// console.log(a.toExponential(3));
+
+// //   3] toFixed()
+
+// const y=23.6553
+// console.log(y.toFixed(2));
+
+// // 4] toprecision()
+
+// const z=23.556
+// console.log(z.toPrecision(2));
+
+
+/************************* MATH **************************/
+
+console.log(Math);
+console.log(Math.sign(-4));
