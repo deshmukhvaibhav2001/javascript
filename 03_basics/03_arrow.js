c564ca52503499c19e0be198f7b47a0e49cf217d
Vaibhav Balasaheb Deshmukh <156877803+deshmukhvaibhav2001@users.noreply.github.com>
2024-02-08 04:57:46 +0000

    container config done

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -4,6 +4,7 @@ const user={
 
     welcomeMessage:function(){
         console.log(`${this.username} , welcome to website`);
+        console.log(this); //this shows current context
     }
 
 }
@@ -13,5 +14,22 @@ user.welcomeMessage()
 user.username="sam"
 
 user.welcomeMessage()
-
+console.log(this);
  
+
+//arrow function
+
+const chai =()=>{
+    let username="hitesh"
+    password=1234567
+}
+
+
+// implicit return
+
+const addTwo =(num1,num2)=>num1 +num2
+
+console.log(addTwo(3,8));
+
+
+
