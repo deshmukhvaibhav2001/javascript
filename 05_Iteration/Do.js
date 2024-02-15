1d2b51ec800280714c0c2a869db7a998439b0598
Vaibhav Balasaheb Deshmukh <156877803+deshmukhvaibhav2001@users.noreply.github.com>
2024-02-15 05:54:15 +0000

    container config done

---
+++ 05_Iteration/Do.js
@@ -0,0 +1,20 @@
+let index=0
+while (index<=10) {
+    console.log(`Value of index is :${index} `);
+    index=index+2
+}
+
+let MyArray =["flash","batsman","superman"]
+
+let arr=0
+while (arr<MyArray.length) {
+    console.log(MyArray);
+    arr=arr+1;
+    
+}
+let score =11
+
+do{
+    console.log(score);
+    score++
+}while(score<=10);
